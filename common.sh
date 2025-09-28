965fa4e358e528df8120278c5aedb74f06def9bb
Jeremy | MekongLabs <jeremy@mekonglabs.tech>
2025-09-28 10:55:48 +0700

    Create common.sh

---
+++ common.sh
@@ -0,0 +1,37 @@
+GREEN="\e[1m\e[1;32m" # green color
+RED="\e[1m\e[1;31m" # red color
+BLUE='\033[0;34m'   # blue color
+YELLOW="\e[1m\e[1;33m"  # yellow color
+NC="\e[0m"          # no color
+
+function printLogo {
+  bash <(curl -s https://raw.githubusercontent.com/Mekong-labs/NodeGuide/main/logo.sh)
+}
+
+function printLine {
+  echo "------------------------------------------------------------------------------------"
+}
+
+function printGreen {
+  echo -e "${GREEN}${1}${NC}"
+}
+
+function printRed {
+  echo -e "${RED}${1}${NC}"
+}
+
+function printBlue {
+  echo -e "${BLUE}${1}${NC}"
+}
+
+function printYellow {
+  echo -e "${YELLOW}${1}${NC}"
+}
+
+function addToPath {
+  source $HOME/.bashrc
+  PATH_EXIST=$(grep "${1}" $HOME/.bashrc)
+  if [ -z "$PATH_EXIST" ]; then
+    echo "export PATH=\$PATH:${1}" >> $HOME/.bashrc
+  fi
+}
